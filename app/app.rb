0f871024a0279ac35a5e268f2d5f101aea30436f
Cichol <cichol@live.cn>
2015-02-26 20:39:18 +0800

    update

--- app/app.rb
+++ app/app.rb
@@ -2,7 +2,7 @@ module CicholBlog
   class App < Padrino::Application
     register ScssInitializer
     use ConnectionPoolManagement
-    register Padrino::Mailer
+    #register Padrino::Mailer
     register Padrino::Helpers
 
     enable :sessions
